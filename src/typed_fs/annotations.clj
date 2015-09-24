4758d6b32ab606b2b58d7ab661d12065d4eefa08
Chris <chris.cornelison@gmail.com>
2015-09-24 16:14:01 -0500

    Ongoing work

--- src/typed_fs/annotations.clj
+++ src/typed_fs/annotations.clj
@@ -1,16 +1,67 @@
 (ns typed-fs.annotations
   (:require [me.raynes.fs :as fs]
-            [clojure.core.typed :as t :refer [ann IFn U Option]])
-  (:import [java.io File]))
+            [clojure.java.io :as io]
+            [clojure.core.typed :as t :refer [ann IFn U Option Bool]])
+  (:import [java.io File]
+           [java.net URI URL]))
 
+(t/defalias ResourceNamish (U File URI URL String))
 
-(ann ^:no-check me.raynes.fs/temp-dir (IFn [String -> (Option File)]
-                                           [String String -> (Option File)]
-                                           [String String Integer -> (Option File)]))
+(ann ^:no-check me.raynes.fs/*cwd* File)
+(ann ^:no-check me.raynes.fs/absolute [ResourceNamish -> File])
+(ann ^:no-check me.raynes.fs/absolute? [ResourceNamish -> Bool])
+(ann ^:no-check me.raynes.fs/base-name (IFn [ResourceNamish -> String]
+                                            [ResourceNamish (U String Bool) -> String]))
+;(ann chdir) skipping
+(ann ^:no-check me.raynes.fs/chmod [String ResourceNamish -> String])
+(ann ^:no-check me.raynes.fs/child-of? [ResourceNamish ResourceNamish -> Bool])
+(ann ^:no-check me.raynes.fs/file (Fn [ResourceNamish String * -> File]))
+
+; technically this function returen (Option File). nil is the function 
+; after some number of tries, fails to create the directory. I'm intentionally
+; ignoring this fact here as it causes too many headaches when passing the return
+; of this function to any of the other function that expect File.
+(ann ^:no-check me.raynes.fs/temp-dir (IFn [String -> File]
+                                           [String String -> File]
+                                           [String String Integer -> File]))
 
 
 (defn foo
   []
-  (let [^File tmpdir (fs/temp-dir "pre")]
-    (when tmpdir (.deleteOnExit tmpdir))))
+  (let [^File tmpdir1 (fs/temp-dir "pre")
+        _ (.deleteOnExit tmpdir1)
+        ^File tmpdir2 (fs/temp-dir "pre" "suf")
+        _ (.deleteOnExit tmpdir2)
+        ^File tmpdir3 (fs/temp-dir "pre" "suf" 100)
+        _ (.deleteOnExit tmpdir3)
+        ^File f fs/*cwd*]
+
+    (let [f (fs/file "foo")
+          uri (URI. "file:///foo")
+          url (URL. "file:///foo")]
+      (fs/file f "foo")
+      (fs/file uri)
+      (fs/file url)
+
+      (fs/absolute "foo")
+      (fs/absolute f)
+      (fs/absolute uri)
+      (fs/absolute url)
+
+      (fs/absolute? "foo")
+      (fs/absolute? f)
+      (fs/absolute? uri)
+      (fs/absolute? url)
+
+      (fs/base-name uri)
+      (fs/base-name url)
+      (fs/base-name "/root/foo.txt")
+      (fs/base-name f)
+      (fs/base-name f true)
+      (fs/base-name f ".txt")
+
+      (fs/child-of? f f)
+      (fs/chmod "+x" tmpdir3)
+      )
+    ))
   
